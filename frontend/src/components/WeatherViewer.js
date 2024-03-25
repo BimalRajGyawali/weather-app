8806a6f2efa7808330fe796000aa405541e0fe13
Bimal <gyawali.rajbimal35@gmail.com>
2024-03-25 11:53:10 +0545

    feat: make api call for current weather with error handling

--- frontend/src/components/WeatherViewer.js
+++ frontend/src/components/WeatherViewer.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Container, Typography, Card, CardContent, Divider} from '@mui/material';
 import {WiThermometer, WiHumidity, WiStrongWind} from 'react-icons/wi'; // Import icons from react-icons library
 
-const dummyCurrentWeather = {
-    location: 'New York',
-    time: '12:00 PM',
-    temperature: 25,
-    humidity: 60,
-    windSpeed: 3,
-    icon: '01d' // Example icon code
-};
+import './weatherviewer.css'
+import axios from "axios";
+import urlConstants from "../constants/UrlConstants";
+import UrlConstants from "../constants/UrlConstants";
+
 
 const dummyHourlyForecast = [
     {time: '12 PM', temperature: 26, icon: '01d'},
@@ -54,6 +51,49 @@ const dummyDailyForecast = [
 ];
 
 const WeatherViewer = () => {
+    const [userLocation, setUserLocation] = useState(null);
+    const [currentWeatherError, setCurrentWeatherError] = useState('')
+
+    const [currentWeather, setCurrentWeather] = useState({})
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const { latitude, longitude } = position.coords;
+                setUserLocation({ latitude, longitude });
+
+                axios.get(`${UrlConstants.HOST}/weather?lat=${latitude}&lon=${longitude}`)
+                    .then(response => {
+                        const weatherData = response.data;
+
+                        const currentWeather = {
+                            location: weatherData.location,
+                            time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}),
+                            temperature: weatherData.temperature,
+                            humidity: weatherData.humidity,
+                            windSpeed: weatherData.wind_speed,
+                            icon: weatherData.icon,
+                            desc: weatherData.desc,
+                            feelsLike: weatherData.feels_like
+                        };
+
+                        setCurrentWeather(currentWeather)
+                        console.log('Received Weather Data:', currentWeather);
+                    })
+                    .catch(error => {
+                        console.error('Error fetching weather data:', error.response ? error.response.data : error.message);
+                        setCurrentWeatherError('Error fetching weather data from server. Please contact support.')
+                    });
+
+
+            }, (error) => {
+                setCurrentWeatherError('Unable to get User Location. Please make sure location is allowed.')
+                console.error('Error getting user location:', error);
+            });
+        } else {
+            console.error('Geolocation is not supported by this browser.');
+        }
+    }, []);
     return (
         <div style={{padding: '20px'}}>
             <Container maxWidth="md">
@@ -65,47 +105,59 @@ const WeatherViewer = () => {
                         </Typography>
                         <Divider style={{marginBottom: '30px', marginTop: '10px'}}/>
 
-                        <Typography variant="h5" gutterBottom style={{marginBottom: '30px'}}>
-                            {dummyCurrentWeather.location} : {dummyCurrentWeather.time}
-                        </Typography>
+                        {
+                            currentWeatherError ? <p>{currentWeatherError}</p>
 
-                        <div style={{display: 'flex', flexDirection: 'row'}}>
-
-                            <div style={{display: 'flex', marginRight: '100px', alignItems: 'center'}}>
-
-                                <img src={`http://openweathermap.org/img/w/${dummyCurrentWeather.icon}.png`}
-                                     alt="Weather Icon"
-                                     style={{width: '72px', height: '72px', border: '1px solid gray'}}/>
-
-                                <div>
-                                    <Typography variant="h4" gutterBottom
-                                                style={{fontWeight: 'bold', marginLeft: '25px'}}>
-                                        {dummyCurrentWeather.temperature}°<span
-                                        style={{fontSize: '0.5em', color: 'gray'}}>C</span>
+                                :
+                                <>
+                                    <Typography variant="h5" gutterBottom style={{marginBottom: '30px'}}>
+                                        {currentWeather.location}, {currentWeather.time}
                                     </Typography>
-                                </div>
-                            </div>
+
+                                    <div style={{display: 'flex', flexDirection: 'row'}}>
+
+                                        <div style={{display: 'flex', marginRight: '100px', alignItems: 'center'}}>
+
+                                            <img src={`http://openweathermap.org/img/w/${currentWeather.icon}.png`}
+                                                 alt="Weather Icon"
+                                                 style={{width: '72px', height: '72px', border: '1px solid gray'}}/>
+
+                                            <div>
+                                                <Typography variant="h4" gutterBottom
+                                                            style={{fontWeight: 'bold', marginLeft: '25px'}}>
+                                                    {currentWeather.temperature}°<span
+                                                    style={{fontSize: '0.5em', color: 'gray'}}>C</span>
+                                                </Typography>
+                                            </div>
+                                        </div>
 
 
-                            <div style={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                marginBottom: '10px',
-                                marginRight: '60px'
-                            }}>
-                                <WiHumidity size={32} style={{marginRight: '8px'}}/>
-                                <Typography variant="h8" gutterBottom style={{fontSize: '1.2em'}}>
-                                    {dummyCurrentWeather.humidity}%
-                                </Typography>
-                            </div>
+                                        <div style={{
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            marginBottom: '10px',
+                                            marginRight: '60px'
+                                        }}>
+                                            <WiHumidity size={32} style={{marginRight: '8px'}}/>
+                                            <Typography variant="h8" gutterBottom style={{fontSize: '1.2em'}}>
+                                                {currentWeather.humidity}%
+                                            </Typography>
+                                        </div>
 
-                            <div style={{display: 'flex', alignItems: 'center'}}>
-                                <WiStrongWind size={32} style={{marginRight: '8px'}}/>
-                                <Typography variant="h8" gutterBottom style={{fontSize: '1.2em'}}>
-                                    {dummyCurrentWeather.windSpeed} m/s
-                                </Typography>
-                            </div>
-                        </div>
+                                        <div style={{display: 'flex', alignItems: 'center'}}>
+                                            <WiStrongWind size={32} style={{marginRight: '8px'}}/>
+                                            <Typography variant="h8" gutterBottom style={{fontSize: '1.2em'}}>
+                                                {isSecureContext.windSpeed} m/s
+                                            </Typography>
+                                        </div>
+                                    </div>
+
+                                </>
+
+
+
+
+                        }
                     </CardContent>
                 </Card>
 
@@ -122,6 +174,7 @@ const WeatherViewer = () => {
                                 justifyContent: 'flex-start',
                                 marginBottom: '20px'
                             }}
+                            className='custom-scrollbar'
 
                         >
 
@@ -226,6 +279,7 @@ const WeatherViewer = () => {
                         </div>
                     </CardContent>
                 </Card>
+
             </Container>
         </div>
     );
---
+++ frontend/src/constants/UrlConstants.js
@@ -0,0 +1,6 @@
+const UrlConstants = {
+    HOST: 'http://localhost:5000'
+}
+
+
+export default UrlConstants;
